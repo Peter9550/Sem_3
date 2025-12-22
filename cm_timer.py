c0d54e3305e48104bd15332da6725b10ef1f7fe9
Peter9550 <165246857+Peter9550@users.noreply.github.com>
2025-12-22 18:01:42 +0300

    Add context managers for timing code execution

---
+++ cm_timer.py
@@ -0,0 +1,31 @@
+# lab_python_fp/cm_timer.py
+import time
+from contextlib import contextmanager
+
+
+class cm_timer_1:
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        end_time = time.time()
+        print('time: {}'.format(end_time - self.start_time))
+
+
+@contextmanager
+def cm_timer_2():
+    start_time = time.time()
+    try:
+        yield
+    finally:
+        end_time = time.time()
+        print('time: {}'.format(end_time - start_time))
+
+
+if __name__ == '__main__':
+    with cm_timer_1():
+        time.sleep(0.5)
+
+    with cm_timer_2():
+        time.sleep(0.5)
