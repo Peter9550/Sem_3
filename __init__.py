27ebbe4fa525ac160dc9e29bde575dc4340d11e4
Peter9550 <165246857+Peter9550@users.noreply.github.com>
2025-12-22 17:50:02 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .abstract_figure import GeometricFigure
+from .color import FigureColor
+from .rectangle import Rectangle
+from .circle import Circle
+from .square import Square
+
+__all__ = ["GeometricFigure", "FigureColor", "Rectangle", "Circle", "Square"]
