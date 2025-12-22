98e9f49be83baad4054cf8d700f94a704ca4f7fd
Peter9550 <165246857+Peter9550@users.noreply.github.com>
2025-12-22 18:02:50 +0300

    Add random number generator function

---
+++ gen_random.py
@@ -0,0 +1,9 @@
+# lab_python_fp/gen_random.py
+import random
+
+def gen_random(num_count, begin, end):
+    for _ in range(num_count):
+        yield random.randint(begin, end)
+
+if __name__ == '__main__':
+    print(list(gen_random(5, 1, 3)))
