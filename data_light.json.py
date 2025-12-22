4d8d819b92e0b1e24a80c945530b0cafecbc9682
Peter9550 <165246857+Peter9550@users.noreply.github.com>
2025-12-22 17:58:34 +0300

    Create data_light.json.py

---
+++ data_light.json.py
@@ -0,0 +1,10 @@
+[
+  {"job-name": "Программист Python", "salary": 100000, "region": "Москва"},
+  {"job-name": "Программист C#", "salary": 120000, "region": "Санкт-Петербург"},
+  {"job-name": "Менеджер по продажам", "salary": 50000, "region": "Москва"},
+  {"job-name": "программист Java", "salary": 130000, "region": "Екатеринбург"},
+  {"job-name": "Дизайнер", "salary": 60000, "region": "Москва"},
+  {"job-name": "Программист 1C", "salary": 90000, "region": "Казань"},
+  {"job-name": "программист python", "salary": 110000, "region": "Новосибирск"},
+  {"job-name": "Аналитик данных", "salary": 150000, "region": "Москва"}
+]
